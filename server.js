cc399e890095e3fd3049a7cd07f9a2efdd6a0799
veznlee <veznlee@qq.com>
2018-10-17 17:21:53 +0800

    '添加文件上传接口'

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require("express");
-var bodyParser = require("body-parser"); 
+var bodyParser = require("body-parser");
 var app = express(); 
 
 //文件读取依赖
 var fs = require('fs');
 var marked = require( "marked" );
+
+//文件上传依赖
+var multer = require('multer');
+const UPLOAD_PATH = './uploads';
+var upload = multer({ dest: UPLOAD_PATH });
 
 var hostName = '127.0.0.1';
 var port = 1024;
@@ -18,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));// 解析表单 application/x
 
 // app.use()用于在访问到最终目标之前做一些事情
 app.use(function (req,res,next) {//不调用next就不继续往下走
-    console.log("做点啥");
+    console.log("请求来啦，在处理之前刷一下存在感");
     next();
 });
 
@@ -50,7 +55,7 @@ app.post("/post",function(req,res){
     res.send(result);
 });
 
-
+//读取文件返回txt
 app.post('/file', function(req, res) {
     var path="./file/test.txt";
     fs.readFile(path, function(err, data){
@@ -66,7 +71,7 @@ app.post('/file', function(req, res) {
     });
 });
 
-
+//读取文件返回json
 app.post('/json', function(req, res) {
     //文件路径，__dirname为当前运行js文件的目录
     //var path = path.join(__dirname, 'file/test.json');
@@ -108,10 +113,136 @@ app.post('/json', function(req, res) {
     });
 });
 
+/* 
+//单个文件上传
+app.post('/upload', upload.single('fileUpload'), function (req, res, next) {
+    const { file } = req;
+    fs.readFile(file.path, function(err, data) {
+        fs.writeFile(`${UPLOAD_PATH}/${file.originalname}`, data, function (err) {
+            if (err){
+                res.json({ 
+                    code:0,
+                    file:file,
+                    msg:err 
+                })
+            }else{
+                res.json({
+                    code:0,
+                    file:file,
+                    msg: '上传成功'
+                });
+            }
+        });
+    })
+})
+
+
+//多文件上传
+app.post('/multipartUpload', upload.array('fileUpload'), function (req, res, next) {
+    const files  = req.files;
+    const success = [];
+    const fail = [];
+    const result = new Promise((resolve, reject) => {
+        files.map((v) => {
+            fs.readFile(v.path, function(err, data) {
+                fs.writeFile(`${UPLOAD_PATH}/${v.originalname}`, data, function(err, data) {
+                    if (err){
+                        reject(err);
+                        fail.push({file:v});
+                    }else{
+                        success.push({file:v});
+                        resolve('成功');
+                    }
+                })
+            })
+        })
+    });
+    result.then(r => {
+        res.json({
+            code:0,
+            success:success,
+            fail:fail,
+            msg: '上传成功'
+        })
+    }).catch(err => {
+        res.json({
+            code:-1,
+            success:success,
+            fail:fail,
+            msg: err 
+        })
+    });
+}) 
+*/
+
+
+//文件上传所需代码
+//设置文件上传路径和文件命名
+var storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        //文件上传成功后会放入uploads文件夹
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb){
+        //设置文件的名字为其原本的名字，也可以添加其他字符，来区别相同文件，例如file.originalname+new Date().getTime();利用时间来区分
+        cb(null, file.originalname)
+    }
+});
+var upload = multer({storage: storage});
+
+//处理来自页面的ajax请求。single文件上传
+app.post('/upload', upload.single('file'), function (req, res, next) {
+    //拼接文件上传后的网络路径
+	console.log("file:"+req.file.originalname)
+    //res.end(req.file.originalname);
+    res.end({
+        code:0,
+        file:req.file.originalname,
+        path:'uploads/'+req.file.originalname,
+        msg:''
+    });
+    // fs.readFile(file.path, function(err, data) {
+    //     fs.writeFile(`${UPLOAD_PATH}/${file.originalname}`, data, function (err) {
+    //         if (err){
+    //             res.json({ 
+    //                 code:0,
+    //                 file:file,
+    //                 msg:err 
+    //             })
+    //         }else{
+    //             res.json({
+    //                 code:0,
+    //                 file:file,
+    //                 msg: '上传成功'
+    //             });
+    //         }
+    //     });
+    // })
+});
+ 
+// 单域多文件上传：input[file]的 multiple=="multiple"
+app.post('/multipartUpload', upload.array('file', 5), function(req, res, next) {
+    // req.files 是前端表单name=="imageFile" 的多个文件信息（数组）,限制数量5，应该打印看一下
+    var fileName = "";
+    for (var i = 0; i < req.files.length; i++) {
+        // 图片会放在uploads目录并且没有后缀，需要自己转存，用到fs模块
+        // 对临时文件转存，fs.rename(oldPath, newPath,callback);
+        fileName+=req.files[i].originalname+";"
+        fs.rename(req.files[i].path, "upload/" + req.files[i].originalname, function(err) {
+            if (err) {throw err;}
+            console.log('done!');
+        })
+    }
+
+    res.writeHead(200, {"Access-Control-Allow-Origin": "*"});//允许跨域。。。
+    // req.body 将具有文本域数据, 如果存在的话
+    //  res.end(JSON.stringify(req.files)+JSON.stringify(req.body));
+	console.log("fileName:"+fileName)
+	res.end(fileName)
+})
 
 
 
 app.listen(port,hostName,function(){
-
-   console.log(`服务器运行在http://${hostName}:${port}`);
+   console.log(`服务已开启，访问地址为http://${hostName}:${port}`);
 });
