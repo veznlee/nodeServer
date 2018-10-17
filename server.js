4d100bd57ce0b167b6cc31e8b32de1f9b18c595c
veznlee <veznlee@qq.com>
2018-10-17 18:08:48 +0800

    ''

--- server.js
+++ server.js
@@ -135,11 +135,12 @@ app.post('/upload', upload.single('fileUpload'), function (req, res, next) {
         });
     })
 })
-
+*/
 
 //多文件上传
 app.post('/multipartUpload', upload.array('fileUpload'), function (req, res, next) {
     const files  = req.files;
+    console.log(files);
     const success = [];
     const fail = [];
     const result = new Promise((resolve, reject) => {
@@ -158,22 +159,23 @@ app.post('/multipartUpload', upload.array('fileUpload'), function (req, res, nex
         })
     });
     result.then(r => {
-        res.json({
-            code:0,
-            success:success,
-            fail:fail,
-            msg: '上传成功'
-        })
+        // res.json({
+        //     code:0,
+        //     success:success,
+        //     fail:fail,
+        //     msg: '上传成功'
+        // });
+        console.log(success);
     }).catch(err => {
-        res.json({
-            code:-1,
-            success:success,
-            fail:fail,
-            msg: err 
-        })
+        // res.json({
+        //     code:-1,
+        //     success:success,
+        //     fail:fail,
+        //     msg: err 
+        // });
+        console.log(fail);
     });
-}) 
-*/
+})
 
 
 //文件上传所需代码
@@ -195,7 +197,7 @@ app.post('/upload', upload.single('file'), function (req, res, next) {
     //拼接文件上传后的网络路径
 	console.log("file:"+req.file.originalname)
     //res.end(req.file.originalname);
-    res.end({
+    res.json({
         code:0,
         file:req.file.originalname,
         path:'uploads/'+req.file.originalname,
@@ -221,25 +223,27 @@ app.post('/upload', upload.single('file'), function (req, res, next) {
 });
  
 // 单域多文件上传：input[file]的 multiple=="multiple"
-app.post('/multipartUpload', upload.array('file', 5), function(req, res, next) {
-    // req.files 是前端表单name=="imageFile" 的多个文件信息（数组）,限制数量5，应该打印看一下
-    var fileName = "";
-    for (var i = 0; i < req.files.length; i++) {
-        // 图片会放在uploads目录并且没有后缀，需要自己转存，用到fs模块
-        // 对临时文件转存，fs.rename(oldPath, newPath,callback);
-        fileName+=req.files[i].originalname+";"
-        fs.rename(req.files[i].path, "upload/" + req.files[i].originalname, function(err) {
-            if (err) {throw err;}
-            console.log('done!');
-        })
-    }
+// app.post('/multipartUpload', upload.array('fileupload', 5), function(req, res, next) {
+//     // req.files 是前端表单name=="imageFile" 的多个文件信息（数组）,限制数量5，应该打印看一下
+//     var fileName = "";
+//     console.log(req.file);
+//     console.log(req.files);
+//     // for (var i = 0; i < req.files.length; i++) {
+//     //     // 图片会放在uploads目录并且没有后缀，需要自己转存，用到fs模块
+//     //     // 对临时文件转存，fs.rename(oldPath, newPath,callback);
+//     //     fileName+=req.files[i].originalname+";"
+//     //     fs.rename(req.files[i].path, "uploads/" + req.files[i].originalname, function(err) {
+//     //         if (err) {throw err;}
+//     //         console.log('done!');
+//     //     })
+//     // }
 
-    res.writeHead(200, {"Access-Control-Allow-Origin": "*"});//允许跨域。。。
-    // req.body 将具有文本域数据, 如果存在的话
-    //  res.end(JSON.stringify(req.files)+JSON.stringify(req.body));
-	console.log("fileName:"+fileName)
-	res.end(fileName)
-})
+//     res.writeHead(200, {"Access-Control-Allow-Origin": "*"});//允许跨域。。。
+//     // req.body 将具有文本域数据, 如果存在的话
+//     //  res.end(JSON.stringify(req.files)+JSON.stringify(req.body));
+// 	console.log("fileName:"+fileName)
+// 	res.end(fileName)
+// })
 
 
 
